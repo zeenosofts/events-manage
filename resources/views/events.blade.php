e0a20a32a6c67b5ad51883bcc1374ee3c30dc903
Muhammad Zohaib <zohaibazhar359@gmail.com>
2022-04-16 23:56:55 +0500

    updated

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -72,9 +72,30 @@
             /* Small devices (portrait tablets and large phones, 600px and up) */
             @media only screen and (min-width: 600px) {
                 h3 {
-                    font-size: 17px;
+                    font-size: 28px;
                 }
             }
+            /* Medium devices (landscape tablets, 768px and up) */
+            @media only screen and (min-width: 768px) {
+                h3 {
+                    font-size: 30px;
+                }
+            }
+
+            /* Large devices (laptops/desktops, 992px and up) */
+            @media only screen and (min-width: 992px) {
+                h3 {
+                    font-size: 32px;
+                }
+            }
+
+            /* Extra large devices (large laptops and desktops, 1200px and up) */
+            @media only screen and (min-width: 1200px) {
+                h3 {
+                    font-size: 32px;
+                }
+            }
+
         </style>
     </head>
 <body>
